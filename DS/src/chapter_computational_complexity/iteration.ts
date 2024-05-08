7a66283c1381ffa03755a583c051af5ac8999556
zhangyupeng <zhangyupeng@haotaisoft.com>
2024-05-08 18:01:59 +0800

    'iteration'

--- DS/src/chapter_computational_complexity/iteration.ts
+++ DS/src/chapter_computational_complexity/iteration.ts
@@ -13,7 +13,7 @@
  * @param n - The number up to which the sum is calculated.
  * @returns The sum of numbers from 1 to n.
  */
-const forLoop = (n: number) => {
+const forLoop = (n: number): number => {
     let res = 0;
     // 循环求和 1, 2, ..., n-1, n
     for (let i = 1; i <= n; i++) {
@@ -22,9 +22,70 @@ const forLoop = (n: number) => {
     return res;
 };
 
+/**
+ * Calculates the sum of numbers from 1 to n using a while loop.
+ * @param n - The number up to which the sum is calculated.
+ * @returns The sum of numbers from 1 to n.
+ */
+const whileLoop = (n: number): number => {
+    let res = 0;
+    let i = 1;
+    while (i <= n) {
+        res += i;
+        i++;
+    }
+    return res;
+};
+
+/**
+ * Calculates the sum of numbers in a specific pattern using a while loop.
+ * The pattern starts with 1 and doubles the increment at each iteration.
+ *
+ * @param n - The number of iterations to perform.
+ * @returns The sum of numbers in the pattern.
+ */
+const whileLoopII = (n: number): number => {
+    let res = 0;
+    let i = 1;
+    // 循环求和 1, 4, 10, ...
+    while (i <= n) {
+        res += i;
+        i++;
+        i *= 2;
+    }
+    return res;
+};
+
+/**
+ * Generates a string representation of nested for loops.
+ * 
+ * @param n - The number of iterations for the outer and inner loops.
+ * @returns A string representation of the nested for loops.
+ */
+const nestedForLoop = (n: number): string => {
+    let res = '';
+    // 循环 i = 1, 2, ..., n-1, n
+    for (let i = 1; i <= n; i++) {
+        // 循环 j = 1, 2, ..., n-1, n
+        for (let j = 0; j <= n; j++) {
+            res += `(${i}, ${j}), `;
+        }
+    }
+    return res;
+};
+
 /**Drive Code */
 const n = 5;
-let res: number;
+let res: number | string;
 
 res = forLoop(n);
-console.log(`first loop sum res = ${res}`);
+console.log(`forLoop sum res = ${res}`);
+
+res = whileLoop(n);
+console.log(`whileLoop sum res = ${res}`);
+
+res = whileLoopII(n);
+console.log(`WhileLoopII sum res = ${res}`);
+
+res = nestedForLoop(n);
+console.log(`nestedForLoop res = ${res}`);
